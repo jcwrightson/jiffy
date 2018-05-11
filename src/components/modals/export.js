82e22d727964779626ddb46c62e6bcced49f81ea
jcwrightson <jcwrightson@gmail.com>
2018-05-11 19:40:05 +0200

    expand / collapse

--- src/components/modals/export.js
+++ src/components/modals/export.js
@@ -1,34 +1,47 @@
 import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
 import store from '../../store'
+
+import { copyToClipboard } from "../../functions";
 
 export default class ExportModal extends Component{
     constructor(props){
         super(props)
 
-        this.state={}
+        this.state={
+            copied: false
+        }
     }
 
     handleClick(e){
         e.preventDefault()
 
-        if(e.target.classList.contains('modal')){
+        if(e.target.classList.contains('modal')) {
             this.props.toggleModal('exportModal')
+
+            this.setState({
+                project : '',
+                copied: false
+            })
+
         }
 
     }
 
     handleCopy(){
-
+        // this.textArea.select()
+        copyToClipboard(this.textArea.value)
+        this.setState({
+            copied: true
+        })
     }
 
     handleChange(e){
-        // console.log(JSON.parse(e.target.value))
 
         this.setState({
             project: e.target.value
         })
 
-        // console.log(this.refs.textArea.val())
     }
 
     handleImport(){
@@ -42,18 +55,19 @@ export default class ExportModal extends Component{
             })
         }
 
-        console.log(props.state)
     }
 
+
     render(){
+
         return(
             <div className={`modal ${this.props.visible ? 'js-active' : ''}`} onClick={this.handleClick.bind(this)}>
                 <div className="inner abs middle">
                     <h2>{this.props.state.import ? 'Import' : 'Export'}</h2>
-                    <textarea value={this.state.project} onChange={this.handleChange.bind(this)} ref='textArea'/>
+                    <textarea value={this.state.project} onChange={this.handleChange.bind(this)} ref={(textArea)=>{this.textArea = textArea}}/>
 
                     {this.props.state.export &&
-                    <button onClick={this.handleCopy.bind(this)}>Copy</button>}
+                    <button onClick={this.handleCopy.bind(this)}>{this.state.copied ? 'Copied' : 'Copy'}</button>}
 
                     {this.props.state.import &&
                     <button onClick={this.handleImport.bind(this)}>Import</button>}
